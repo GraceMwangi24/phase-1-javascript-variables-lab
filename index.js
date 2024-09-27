a77133dda99ce7fb0c9b5f4c965c4ba08e696a22
grace <gracemwangi84@gmail.com>
2024-09-27 19:44:22 +0300

    variables lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+//l Code your solution in this file!n
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
